ba48e75417673c969269520b810e3ff427fe6f55
Dyhoereos <dennis.chau@outlook.com>
2016-10-23 01:56:01 -0700

    Added alert views

--- actinosheetexample/ViewController.swift
+++ actinosheetexample/ViewController.swift
@@ -23,12 +23,97 @@ class ViewController: UIViewController {
     @IBAction func shareActionSheet(_ sender: AnyObject) {
         let actionSheet : UIAlertController = UIAlertController(title: "Log In", message: "How do you want to Log in?", preferredStyle: UIAlertControllerStyle.actionSheet)
         
-        actionSheet.addAction(UIAlertAction(title:"Facebook", style: UIAlertActionStyle.default, handler:{ action in print("Facebook Selected")}))
-        actionSheet.addAction(UIAlertAction(title:"Google+", style: UIAlertActionStyle.default, handler:{ action in print("Google+ Selected")}))
-        actionSheet.addAction(UIAlertAction(title:"Twitter", style: UIAlertActionStyle.default, handler:{ action in print("Twitter Selected")}))
+        func signupHandler(actionTarget: UIAlertAction){
+            let controller = UIAlertController(title: "Registration", message: "Create a new account", preferredStyle: .alert)
+            
+            controller.addTextField(configurationHandler: {(textField : UITextField) -> Void in textField.placeholder = "username"})
+            controller.addTextField(configurationHandler: {(textField : UITextField) -> Void in textField.placeholder = "email"})
+            controller.addTextField(configurationHandler: {(textField : UITextField) -> Void in textField.placeholder = "password";
+                textField.isSecureTextEntry = true})
+            controller.addTextField(configurationHandler: {(textField : UITextField) -> Void in textField.placeholder = "confirm password";
+                textField.isSecureTextEntry = true})
+            
+            let signup = UIAlertAction(title: "Sign Up", style: .default, handler: nil)
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            
+            controller.addAction(signup)
+            controller.addAction(cancel)
+            
+            present(controller, animated: true, completion: nil)
+        }
+        
+        func loginHandler(actionTarget: UIAlertAction){
+            let controller = UIAlertController(title: "Login", message: "Enter username and password", preferredStyle: .alert)
+            
+            controller.addTextField(configurationHandler: {(textField : UITextField) -> Void in textField.placeholder = "username"})
+            
+            controller.addTextField(configurationHandler: {(textField : UITextField) -> Void in textField.placeholder = "password";
+                textField.isSecureTextEntry = true})
+            
+            let login = UIAlertAction(title: "Login", style: .default, handler: nil)
+            let forgot = UIAlertAction(title: "Forgot Password", style: .default, handler: nil)
+            let signup = UIAlertAction(title: "Sign Up", style: .default, handler: signupHandler)
+            let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            
+            controller.addAction(login)
+            controller.addAction(forgot)
+            controller.addAction(signup)
+            controller.addAction(cancel)
+            
+            present(controller, animated: true, completion: nil)
+        }
+        
+        actionSheet.addAction(UIAlertAction(title:"Facebook", style: UIAlertActionStyle.default, handler:loginHandler))
+        actionSheet.addAction(UIAlertAction(title:"Google+", style: UIAlertActionStyle.default, handler:loginHandler))
+        actionSheet.addAction(UIAlertAction(title:"Twitter", style: UIAlertActionStyle.default, handler:loginHandler))
         actionSheet.addAction(UIAlertAction(title:"Cancel", style: UIAlertActionStyle.cancel, handler: nil))
         present(actionSheet, animated: true, completion:nil)
-
+        
 
     }
+
+    
+    @IBAction func defaultButtonPressed(_ sender: UIButton) {
+        let controller = UIAlertController(title: "Default Style", message: "the default alert view style", preferredStyle: .alert)
+        let ok = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        controller.addAction(ok)
+        controller.addAction(cancel)
+        
+        present(controller, animated: true, completion: nil)
+    }
+    
+    @IBAction func inputButtonPressed(_ sender: UIButton) {
+        let controller = UIAlertController(title: "Input Fields", message: "Insecure and secure text fields", preferredStyle: .alert)
+        
+        controller.addTextField(configurationHandler: {(textField : UITextField) -> Void in textField.placeholder = "insecure field";})
+        
+        controller.addTextField(configurationHandler: {(textField : UITextField) -> Void in textField.placeholder = "secure field";
+            textField.isSecureTextEntry = true})
+        
+        let ok = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        
+        controller.addAction(ok)
+        controller.addAction(cancel)
+        
+        present(controller, animated: true, completion: nil)
+    }
+    
+    @IBAction func desButtonPressed(_ sender: UIButton) {
+        let controller = UIAlertController(title: "Destructive Action", message: "Launch nuclear missles?", preferredStyle: .alert)
+        
+        let launch = UIAlertAction(title: "Launch", style: .destructive, handler: nil)
+        let other = UIAlertAction(title: "Launch something else", style: .default, handler: nil)
+        let abort = UIAlertAction(title: "Abort", style: .cancel, handler: nil)
+        
+        controller.addAction(launch)
+        controller.addAction(other)
+        controller.addAction(abort)
+        
+        present(controller, animated: false, completion: nil)
+    }
+    
+    
 }
